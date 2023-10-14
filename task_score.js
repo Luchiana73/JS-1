d79114c89103998f2f69dc6f941f9d0622190af3
Svetlana <luchiana.sv@gmail.com>
2023-10-14 21:11:37 +0200

    cteated file task value hw  2.1.5

--- task_score.js
+++ task_score.js
@@ -1,9 +1,15 @@
 let introductionModuleScore = 180;
 let gitModuleScore = 250;
 let jsModuleScore = 380;
-const averageScore = (introductionModuleScore + gitModuleScore + jsModuleScore) / 3;
+const averageScore =
+  (introductionModuleScore + gitModuleScore + jsModuleScore) / 3;
 
-const studentName = 'Светлана';
-const studentSurname = 'Городецкая';
+const studentName = "Светлана";
+const studentSurname = "Городецкая";
 
-console.log(`Средний балл за 3 модуля у студента по имени ${studentName.slice(0, 1)}.${studentSurname} равен ${averageScore}.`);
+console.log(
+  `Средний балл за 3 модуля у студента по имени ${studentName.slice(
+    0,
+    1
+  )}.${studentSurname} равен ${averageScore}.`
+);
