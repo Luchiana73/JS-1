cfdfd5bb22d7963ce1c25a9dce9a0a97a0b632a1
Svetlana <luchiana.sv@gmail.com>
2023-10-14 18:50:41 +0200

    created file task score - hw 2.1.3

---
+++ task_score.js
@@ -0,0 +1,9 @@
+let introductionModuleScore = 180;
+let gitModuleScore = 250;
+let jsModuleScore = 380;
+const averageScore = (introductionModuleScore + gitModuleScore + jsModuleScore) / 3;
+
+const studentName = 'Светлана';
+const studentSurname = 'Городецкая';
+
+console.log(`Средний балл за 3 модуля у студента по имени ${studentName} ${studentSurname} равен ${averageScore}.`);
